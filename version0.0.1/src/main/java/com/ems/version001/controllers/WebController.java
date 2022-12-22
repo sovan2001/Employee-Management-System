27f77ababf84c52b7ac5f984babb2d6fe178e212
sovan2001 <contact.sovan2001@gmail.com>
2022-12-22 21:54:15 +0530

    Connected Login Page with basic CSS and Bootstrap

--- version0.0.1/src/main/java/com/ems/version001/controllers/WebController.java
+++ version0.0.1/src/main/java/com/ems/version001/controllers/WebController.java
@@ -1,5 +1,13 @@
 package com.ems.version001.controllers;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
 public class WebController {
 
+	@RequestMapping("/login")
+	public String login() {
+		return "login";
+	}
 }
